cfeffa62d0f9d48528761071495d8943e7c9e7e8
Mathew Bravo <mathewbravo94@gmail.com>
2025-01-05 17:29:31 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+pub struct Network{
+    layers: Vec<Layer>,
+}
+
+#[derive(Debug)]
+struct Layer{
+    neurons: Vec<Neuron>,
+}
+
+#[derive(Debug)]
+pub struct Neuron{
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+impl Network {
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+impl Layer{
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32>{
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+}
+
+impl Neuron{
+    fn propagate(&self, inputs: &[f32]) -> f32{
+        assert_eq!(inputs.len(), self.weights.len());
+
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
+    }
+}
