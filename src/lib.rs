951a3e034dcee9f9b9ca9e3bfaede5b1d3d4fb84
Mathew Bravo <mathewbravo94@gmail.com>
2025-01-06 20:12:05 -0500

    Added a randomization function and added testing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+// !TODO Fix tests for approx random numbers
+
+
+use rand::{ Rng, RngCore };
+
 #[derive(Debug)]
 pub struct Network{
     layers: Vec<Layer>,
@@ -14,8 +19,21 @@ pub struct Neuron{
     weights: Vec<f32>,
 }
 
+#[derive(Debug)]
+pub struct LayerTopology{
+    pub neurons: usize,
+}
+
 impl Network {
-    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self{
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
+            .collect();
+
+        Self { layers }
+    }
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
@@ -23,6 +41,13 @@ impl Network {
 }
 
 impl Layer{
+    fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self{
+        let neurons = (0..output_size)
+            .map(|_| Neuron::random(rng, input_size))
+            .collect();
+
+        Self {neurons}
+    }
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32>{
         self.neurons
             .iter()
@@ -32,6 +57,15 @@ impl Layer{
 }
 
 impl Neuron{
+    fn random(rng: &mut dyn RngCore, input_size: usize) -> Self{
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        let weights = (0..input_size)
+            .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+
+        Self {bias, weights}
+    }
     fn propagate(&self, inputs: &[f32]) -> f32{
         assert_eq!(inputs.len(), self.weights.len());
 
@@ -42,5 +76,20 @@ impl Neuron{
             .sum::<f32>();
 
         (self.bias + output).max(0.0)
+    }
+}
+
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    #[test]
+    fn random(){
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let neuron = Neuron::random(&mut rng, 4);
+
+        assert_eq!(neuron.bias, -0.6255188);
+        assert_eq!(neuron.weights,  &[0.67383957, 0.8181262, 0.26284897, 0.5238807]);
     }
 }
